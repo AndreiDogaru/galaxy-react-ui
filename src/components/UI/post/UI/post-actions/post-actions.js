a609f0bf05fe1fa16b1834cd2ea029e99eee2912
Andrei D <andrei.d.wiredelta@gmail.com>
2019-10-17 11:00:58 +0200

    added like btn for comments

--- src/components/UI/post/UI/post-actions/post-actions.js
+++ src/components/UI/post/UI/post-actions/post-actions.js
@@ -6,10 +6,10 @@ import './post-actions.scss';
 const PostActions = (props) => {
   const [isLiked, setIsLiked] = useState(false);
 
-  const { toggleComments } = props;
+  const { toggleComments, inComment } = props;
 
   return (
-    <div className="post_actions">
+    <div className={`post_actions ${inComment ? 'post_actions-incomment' : ''}`}>
       <div
         className={isLiked ? 'clickable focused' : 'clickable'}
         onClick={() => setIsLiked((prev) => !prev)}
@@ -22,16 +22,18 @@ const PostActions = (props) => {
         <h4>Like</h4>
       </div>
 
-      <div
-        className="clickable"
-        onClick={toggleComments}
-        onKeyPress={() => { }}
-        role="button"
-        tabIndex={0}
-      >
-        <MessageSquare />
-        <h4>Comment</h4>
-      </div>
+      {!inComment && (
+        <div
+          className="clickable"
+          onClick={toggleComments}
+          onKeyPress={() => { }}
+          role="button"
+          tabIndex={0}
+        >
+          <MessageSquare />
+          <h4>Comment</h4>
+        </div>
+      )}
     </div>
   );
 };
--- src/components/UI/post/UI/post-comments/UI/comment_container/comment_container.js
+++ src/components/UI/post/UI/post-comments/UI/comment_container/comment_container.js
@@ -5,9 +5,10 @@ import './comment_container.scss';
 import ProfileIcon from '../../../../../profile-icon/profile-icon';
 import PostSubheader from '../../../post-subheader/post-subheader';
 import PostText from '../../../post-text/post-text';
-import PostStatistics from '../../../post-statistics/post-statistics';
 import PostFiles from '../../../post-files/post-files';
 import SmallImagesRow from '../../../post-images/UI/small-images-row/small-images-row';
+import PostStatistics from '../../../post-statistics/post-statistics';
+import PostActions from '../../../post-actions/post-actions';
 
 const CommentContainer = (props) => {
   const { data } = props;
@@ -23,22 +24,27 @@ const CommentContainer = (props) => {
     <div className="comment_container">
       <ProfileIcon />
 
-      <div className="comment_content">
-        <PostSubheader inComment menuItems={menuItems} />
+      <div className="comment_column">
+        <div className="comment_content">
+          <PostSubheader inComment menuItems={menuItems} />
 
-        <PostText inComment text={data.text} />
+          <PostText inComment text={data.text} />
 
-        {data.files && data.files.length > 0 && (
-          <PostFiles inComment data={data.files} />
-        )}
+          {data.files && data.files.length > 0 && (
+            <PostFiles inComment data={data.files} />
+          )}
 
-        {data.images && data.images.length > 0 && (
-          <div className="comment_images">
-            <SmallImagesRow inComment data={data.images} />
-          </div>
-        )}
+          {data.images && data.images.length > 0 && (
+            <div className="comment_images">
+              <SmallImagesRow inComment data={data.images} />
+            </div>
+          )}
 
-        <PostStatistics inComment />
+          <PostStatistics inComment />
+
+        </div>
+
+        <PostActions inComment />
       </div>
     </div>
   );
